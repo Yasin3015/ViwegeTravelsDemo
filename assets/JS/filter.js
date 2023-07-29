a8262b6235858993d8a23b78b6172b0a428056b6
Yasin Marei <59691385+Yasin3015@users.noreply.github.com>
2023-07-30 00:33:47 +0300

    Add files via upload

---
+++ assets/JS/filter.js
@@ -0,0 +1,83 @@
+let myCards = document.getElementsByClassName("carde");
+    var arr = new Array(myCards.length);
+    let opj= {
+        Price: 0.0,
+        Diration: '0'
+      }
+    for(let i = 0 ; i<myCards.length ;i++){
+      let price = myCards[i].children[1].children[0].children[1].children[1].children[0].innerHTML;
+      let duration = myCards[i].children[1].children[0].children[1].children[2].innerHTML;
+      opj = {
+        Price: parseFloat(price),
+        Duration: duration
+      }
+      arr[i] = opj;
+    }
+    function filterFun(){
+      let type = document.getElementById("type").value;
+      let tripClass = document.getElementById("tripClass").value;
+      switch(type){
+        case "Excursions (all)":
+          if(tripClass === "Economic (500$ or less)"){
+              for(let i = 0; i<arr.length;i++){
+                if(arr[i].Price <= 500)
+                  myCards[i].style.display="block";
+                else
+                  myCards[i].style.display="none";
+              }
+          }
+          else if(tripClass === "VIP (more than 500$)"){
+              for(let i = 0; i<arr.length;i++){
+                  if(parseFloat(arr[i].Price) > 500)
+                    myCards[i].style.display="block";
+                  else
+                    myCards[i].style.display="none";
+              }
+            
+          }
+        break;
+        case "Transfers (1day)":
+          if(tripClass === "Economic (500$ or less)"){
+              for(let i = 0; i<arr.length;i++){
+                if(arr[i].Price <= 500 && arr[i].Duration === "Duration: 1D")
+                  myCards[i].style.display="block";
+                else
+                  myCards[i].style.display="none";
+              }
+          }
+          else if(tripClass === "VIP (more than 500$)"){
+              for(let i = 0; i<arr.length;i++){
+                  if(arr[i].Price > 500 && arr[i].Duration === "Duration: 1D")
+                    myCards[i].style.display="block";
+                  else
+                    myCards[i].style.display="none";
+              }
+            
+          }
+        break;
+        case "Accommodations (more than 1day)":
+          if(tripClass === "Economic (500$ or less)"){
+              for(let i = 0; i<arr.length;i++){
+                if(arr[i].Price <= 500 && arr[i].Duration !== "Duration: 1D")
+                  myCards[i].style.display="block";
+                else
+                  myCards[i].style.display="none";
+              }
+          }
+          else if(tripClass === "VIP (more than 500$)"){
+              for(let i = 0; i<arr.length;i++){
+                  if(arr[i].Price > 500 && arr[i].Duration !== "Duration: 1D")
+                    myCards[i].style.display="block";
+                  else
+                    myCards[i].style.display="none";
+              }
+            
+          }
+        break;
+
+      }
+    }
+    document.getElementById("filterButton").onclick = function(){
+      filterFun()
+    }
+    filterFun()
